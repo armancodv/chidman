d7ea9fce631c737d98123f881141ac1cb59e6208
akolahan <akolahan@gmail.com>
2015-09-10 19:27:48 +0430

    aa

--- www/script.js
+++ www/script.js
@@ -1,27 +1,3 @@
-var admobid = {};
-if( /(android)/i.test(navigator.userAgent) ) { 
-    admobid = { // for Android
-        banner: 'ca-app-pub-9156926935885120/4939288890',
-        interstitial: 'ca-app-pub-9156926935885120/7253626891'
-    };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-    admobid = { // for iOS
-        banner: 'ca-app-pub-9156926935885120/4939288890',
-        interstitial: 'ca-app-pub-9156926935885120/7253626891'
-    };
-} else {
-    admobid = { // for Windows Phone
-        banner: 'ca-app-pub-9156926935885120/4939288890',
-        interstitial: 'ca-app-pub-9156926935885120/7253626891'
-    };
-}
-
-// preppare and load ad resource in background, e.g. at begining of game level
-if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
-
-// show the interstitial later, e.g. at end of game level
-if(AdMob) AdMob.showInterstitial();
-
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $timeout) {
 
@@ -223,4 +199,113 @@ function next_step(number) {
 	} else {
 		document.getElementById('next-step').style.display='none';
 	}
+}
+
+
+
+function onLoad() {
+    if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+        document.addEventListener('deviceready', initApp, false);
+    } else {
+        initApp();
+    }
+}
+
+var admobid = {};
+if( /(android)/i.test(navigator.userAgent) ) {
+	admobid = {
+		banner: 'ca-app-pub-6869992474017983/9375997553',
+		interstitial: 'ca-app-pub-6869992474017983/1657046752'
+	};
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+	admobid = {
+		banner: 'ca-app-pub-6869992474017983/4806197152',
+		interstitial: 'ca-app-pub-6869992474017983/7563979554'
+	};
+} else {
+	admobid = {
+		banner: 'ca-app-pub-6869992474017983/8878394753',
+		interstitial: 'ca-app-pub-6869992474017983/1355127956'
+	};
+}
+    
+function initApp() {
+if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+initAd();
+    // display a banner at startup
+    createSelectedBanner();
+}
+function initAd(){
+    var defaultOptions = {
+        // bannerId: admobid.banner,
+        // interstitialId: admobid.interstitial,
+        // adSize: 'SMART_BANNER',
+        // width: integer, // valid when set adSize 'CUSTOM'
+        // height: integer, // valid when set adSize 'CUSTOM'
+        position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
+        bgColor: 'black', // color name, or '#RRGGBB'
+        // x: integer,		// valid when set position to 0 / POS_XY
+        // y: integer,		// valid when set position to 0 / POS_XY
+        isTesting: true, // set to true, to receiving test ad for testing purpose
+        // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
+    };
+    AdMob.setOptions( defaultOptions );
+    registerAdEvents();
+}
+// optional, in case respond to events or handle error
+function registerAdEvents() {
+    // new events, with variable to differentiate: adNetwork, adType, adEvent
+    document.addEventListener('onAdFailLoad', function(data){ 
+    	alert('error: ' + data.error + 
+    			', reason: ' + data.reason + 
+    			', adNetwork:' + data.adNetwork + 
+    			', adType:' + data.adType + 
+    			', adEvent:' + data.adEvent); // adType: 'banner' or 'interstitial'
+    });
+    document.addEventListener('onAdLoaded', function(data){});
+    document.addEventListener('onAdPresent', function(data){});
+    document.addEventListener('onAdLeaveApp', function(data){});
+    document.addEventListener('onAdDismiss', function(data){});
+}
+
+// click button to call following functions
+function getSelectedAdSize() {
+    var i = document.getElementById("adSize").selectedIndex;
+    var items = document.getElementById("adSize").options;
+    return items[i].value;
+}
+function getSelectedPosition() {
+    var i = document.getElementById("adPosition").selectedIndex;
+    var items = document.getElementById("adPosition").options;
+    return parseInt( items[i].value );
+}
+function createSelectedBanner() {
+	var overlap = document.getElementById('overlap').checked;
+    var offsetTopBar = document.getElementById('offsetTopBar').checked;
+    AdMob.createBanner( {adId:admobid.banner, overlap:overlap, offsetTopBar:offsetTopBar, adSize: getSelectedAdSize(), position:getSelectedPosition()} );
+}
+function createBannerOfGivenSize() {
+    var w = document.getElementById('w').value;
+    var h = document.getElementById('h').value;
+    
+    AdMob.createBanner( {adId:admobid.banner,
+                       adSize:'CUSTOM', width:w, height:h,
+                       position:getSelectedPosition()} );
+}
+function showBannerAtSelectedPosition() {
+    AdMob.showBanner( getSelectedPosition() );
+}
+function showBannerAtGivenXY() {
+    var x = document.getElementById('x').value;
+    var y = document.getElementById('y').value;
+    AdMob.showBannerAtXY(x, y);
+}
+function prepareInterstitial() {
+    var autoshow = document.getElementById('autoshow').checked;
+    AdMob.prepareInterstitial({adId:admobid.interstitial, autoShow:autoshow});
+}
+function onResize(){
+    var s = document.getElementById('sizeinfo');
+    s.innerHTML = "web view: " + window.innerWidth + " x " + window.innerHeight;
 }
